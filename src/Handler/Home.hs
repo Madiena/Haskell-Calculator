9af52f3fa329ff30cfc137355717fae6bfd85b17
Nina Hammer <nina.hammer@mni.thm.de>
2022-09-04 15:48:13 +0200

    POST to /function now returns JSON with function

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,11 +1,24 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
 module Handler.Home where
 
 import Import
+import Data.Aeson()
+import Yesod.Core.Content()
 
 getHomeR :: Handler ()
-getHomeR = do 
-    sendFile typeHtml "static/index.html"
+getHomeR = do sendFile typeHtml "static/index.html"
 
+newtype Function = Function {
+    function :: Text 
+} deriving (Show, Generic)
+getFuction :: Function -> Text 
+getFuction (Function fun) = fun 
 
+instance ToJSON Function
+instance FromJSON Function
+
+postFunctionR :: Handler Value
+postFunctionR = do
+    fun <- requireInsecureJsonBody :: Handler Value
+    returnJson fun
