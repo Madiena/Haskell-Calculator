125cc0316d2bdf4e3b8518b64b7c4c8632b5b627
Madiena <madelainehestermann@yahoo.de>
2022-09-05 14:24:33 +0200

    Add abstract syntax

--- src/parser/parser.hs
+++ src/parser/parser.hs
@@ -1,6 +1,35 @@
 
 import Text.ParserCombinators.Parsec
 
+function = chainl1 exp1 opLow
+ {-do
+    f <- function
+    op <- opLow
+    rest <- exp1
+    return (f : rest)-}
+
+exp1 = do
+    exp <- exp1
+    op <- opMed
+    rest <- exp2
+    return (exp : rest) <|> exp2
+
+exp2 = do
+    minus <- char '-'
+    rest <- exp3
+    return (minus : rest) <|> exp3
+
+exp3 = do
+    lparen <- char '('
+    f <- function
+    rparen <- char ')'
+    return (lparen : rparen)
+    <|> value
+
+opLow = char '+' <|> char '-'
+
+opMed = char '*' <|> char '/'
+
 zero = char '0'
 dig = oneOf ['1' .. '9']
 number = do
@@ -9,30 +38,6 @@ number = do
     return (c : cs)
 -- : [] = \x --> Lambda wird direkt an fmap übergeben (\x -> [x])
 value = number <|> fmap (: []) (char 'x') 
-opLow = char '+' <|> char '-'
-opMed = char '*' <|> char '/'
-function = 
-    
-    {-do
-    f <- fmap function
-    op <- fmap (: []) opLow
-    rest <- exp1
-    return (f : rest)-}
-exp1 = do
-    exp <- exp1
-    op <- opMed
-    rest <- exp2
-    return (exp : rest) -- <|> exp2
-exp2 = do
-    minus <- char '-'
-    rest <- exp3
-    return (minus : rest) -- <|> exp3
-exp3 = do
-    lparen <- char '('
-    f <- function
-    rparen <- char ')'
-    return (lparen : rparen)
-    <|> value
 
 parseFunction :: String -> Either ParseError String
 parseFunction = parse function "(f)"
