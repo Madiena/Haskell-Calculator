1ee5814639c2147d64b67256bffa2b9e4326f8e4
Madiena <madelainehestermann@yahoo.de>
2022-09-22 18:20:58 +0200

    Attempt to keep old SymbolTable when adding new elements

--- src/Parser/SymbolTable.hs
+++ src/Parser/SymbolTable.hs
@@ -10,4 +10,5 @@ storeDefinition :: Definition -> SymbolTable
 storeDefinition (FunctionDef name params body) =  SymbolTable [(name, (FunctionDef name params body))]
 storeDefinition (VariableDef name body) = SymbolTable [(name, (VariableDef name body))]
     
-    
+addNewDefToStoredList :: Definition -> SymbolTable -> SymbolTable
+addNewDefToStoredList newDef SymbolTable [(name, oldDef)] = SymbolTable [((name, oldDef): (storeDefinition newDef))]
