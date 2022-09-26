69115145a904ffac57d19e91272535f7886f8afa
Nina Hammer <nina.hammer@mni.thm.de>
2022-09-27 00:08:37 +0200

    nullstellen und anbindung an webserver

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -5,6 +5,7 @@ import Import
 import Data.Aeson()
 import Yesod.Core.Content()
 import Parser.Parser
+import Parser.ZeroCrossings
 
 getHomeR :: Handler ()
 getHomeR = do sendFile typeHtml "static/index.html"
@@ -20,5 +21,21 @@ postFunctionR :: Handler Value
 postFunctionR = do
     fun <- requireInsecureJsonBody :: Handler Function
     case parseFunction (fnString fun) of
-       Left err -> returnJson $ Function "error"
+       Left _ -> returnJson $ Function "error"
        Right expr -> returnJson $ Function (compileToJS expr)
+
+data ZeroPoints = ZeroPoints {
+    func :: String,
+    zeroPoints :: [String]
+} deriving (Show, Generic)
+instance ToJSON ZeroPoints
+instance FromJSON ZeroPoints
+
+postZeroR :: Handler Value
+postZeroR = do
+    fun <- requireInsecureJsonBody :: Handler Function
+    case parseFunction $ fnString fun of 
+        Left _ -> returnJson $ ZeroPoints "error" ["error"]
+        Right expr -> returnJson $ ZeroPoints (compileToJS expr) (calculateZeroPoints $ returnExpressionFromDef expr)
+    
+    
--- src/Parser/ZeroCrossings.hs
+++ src/Parser/ZeroCrossings.hs
@@ -1,30 +1,44 @@
 module Parser.ZeroCrossings(replaceIdentifierInExpression, calculateZeroPoints, calculate, returnExpressionFromDef) where
 
 import Parser.AbstractSyntax
-import Data.List (sortOn)
-import Text.ParserCombinators.Parsec
+import Text.Printf
 
+{-
+    Nimmt eine Expression und eine Zahl entgegen und ersetzt jede Variable in der Expression mit dieser Zahl. ("Setzt die Zahl in die Funktion ein")
+-}
 replaceIdentifierInExpression :: Expression -> Double -> Expression
 replaceIdentifierInExpression (Binary op opLeft opRight) x = Binary op (replaceIdentifierInExpression opLeft x) (replaceIdentifierInExpression opRight x)
 replaceIdentifierInExpression (Number d) x = Number d
-replaceIdentifierInExpression (Var name) x = Number x
+replaceIdentifierInExpression (Var _) x = Number x
 
+{-
+    setzt jeden Wert aus der Liste im Zweiten Argument in die Expression aus dem ersten Argument ein und gibt eine Liste aus Tupeln bestehend aus dem eingesetzten x-Wert 
+    und dem damit berechneten y-Wert zurück
+-}
 tryZeroPoints :: Expression -> [Double] -> [(Double, Double)]
 tryZeroPoints ex li = [(d, calculate (replaceIdentifierInExpression ex d)) | d <- li]
 
-findClosest :: [(Double, Double)] -> [(Double, Double)]
-findClosest li = filter ((< 0.00000001) . snd ) [(fst d, abs (snd d)) | d <- li]
---findClosest li = sortOn snd [(fst d, abs (snd d)) | d <- li]
+{-
+    filtert die Liste aus "tryZeroPoints" nach y-Werten, die sehr nahe an 0 sind
+-}
+
+findCloseToZero :: [(Double, Double)] -> [(Double, Double)]
+findCloseToZero li = filter ((< 0.001) . snd ) [(fst d, abs $ snd d) | d <- li]
+
+{-
+    nimmt eine Liste aus Tupeln entgegen und gibt eine Liste bestehend aus dem ersten Double zurück
+-}
+
+mapTupleToFirstString :: [(Double, Double)] -> [String]
+mapTupleToFirstString tupLst = [printf "%.2f" $ fst d | d <- tupLst]
 
 
-
-calculateZeroPoints :: Expression -> [(Double, Double)]
-calculateZeroPoints ex = findClosest $ tryZeroPoints ex [-10.0,-9.999..10.0 ]
---calculateZeroPoints ex = sortOn snd $ tryZeroPoints ex [-10.0,-9.9..10.0 ]
+calculateZeroPoints :: Expression -> [String]
+calculateZeroPoints ex = mapTupleToFirstString $ findCloseToZero $ tryZeroPoints ex [-10.0,-9.999..10.0 ]
 
 returnExpressionFromDef :: Definition -> Expression
 -- returnExpressionFromDef (def :: FunctionDef) = funcBody def
-returnExpressionFromDef (FunctionDef { funcBody = funcBody }) = funcBody  
+returnExpressionFromDef FunctionDef { funcBody = funcBody } = funcBody  
 
 -- Unterscheidung zwischen Division durch 0 und anderen Operationen funktioniert!
 -- Möglichkeit ParseError oder Ähnliches abzubilden fehlt -
@@ -55,7 +69,7 @@ calculate _ = undefined
 -}
 
 calculate :: Expression -> Double
-calculate (Binary op opLeft opRight) = (operator op (calculate opLeft) (calculate opRight))
+calculate (Binary op opLeft opRight) = operator op (calculate opLeft) (calculate opRight)
 calculate (Number d) = d 
 --calculate (Var v) = -- replaceIdentifierInExpression v  --  f(x) = x*x + 2*x   5      
 calculate _ = undefined
